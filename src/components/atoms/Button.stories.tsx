118bae1f5f8677bf8d4a7e02c756256e575317fc
wlsrn3684 <wlsrn3684@naver.com>
2023-02-06 17:22:46 +0900

    🐛 Fix storybook tailwind load bug

---
+++ src/components/atoms/Button.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import Button from "./Button";
+
+export default {
+  title: "Atom/Button",
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+export const Default = Template.bind({});
+
+Default.args = {
+    children: "Default"
+};
