60708600fabdbaacb6511b661a0a0db22d6963b4
SeungHo0422 <songseungho9258@gmail.com>
2023-02-11 22:47:21 +0900

    ✨ Add Lobby Layout
    로비페이지 레이아웃 추가

---
+++ src/layout/Lobby/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <header>
+        <h2>This is Header</h2>
+        {/* css 중에 transform - scale 찾아보자. (배율 주기) */}
+    </header>
+  );
+};
+
+export default Header;
+
