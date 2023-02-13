9a4de9c0063467ca0d8b217300e0f460dc3fbddc
SeungHo0422 <songseungho9258@gmail.com>
2023-02-14 03:24:56 +0900

    ✨ Add some Buttons

--- src/pages/LobbyPage.tsx
+++ src/pages/LobbyPage.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import Button from '../components/atoms/Button/Button'
 
 const LobbyPage = () => {
   return (
-    <></>
+    <>
+    <div className='justify-center mt-20'>
+      <Button className='relative mx-12 justify-center'>먼저 끝까지 도달하기</Button>
+      <Button className='relative mx-12 justify-center'>N전M승</Button>
+      <Button className='relative mx-12 justify-center'>1문제 배틀</Button>
+    </div>
+    </>
   );
 };
 
