b50e65dfc1dbf570bf9da1960cdd3a56a9280116
SeungHo0422 <songseungho9258@gmail.com>
2023-02-14 02:51:38 +0900

    🚚 Add LobbyPage routes path

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import GameLayout from "./layout/GameLayout";
-import LobbyLayout from "./layout/LobbyLayout";
+import LobbyLayout from "./layout/Lobby/LobbyLayout";
 
 import GamePage from "./pages/GamePage";
 import LobbyPage from "./pages/LobbyPage";
@@ -12,7 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/lobby" element={<LobbyLayout />}>
-          <Route path="" element={<LobbyPage />} />
+          <Route path="gameSelect" element={<LobbyPage />} />
         </Route>
         <Route path="/game" element={<GameLayout />}>
           <Route path="" element={<GamePage />} />
