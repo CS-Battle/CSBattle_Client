af8ef9b38481784c13e63d458c3a979c685885e2
wlsrn3684 <wlsrn3684@naver.com>
2023-03-05 17:18:26 +0900

    ✨ Redirect userId does not exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import React from "react"
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { redirect, BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
-import GameLayout from "./layout/GameLayout"
-import LobbyLayout from "./layout/LobbyLayout"
+import GameLayout from "layout/GameLayout"
+import LobbyLayout from "layout/Lobby/LobbyLayout"
 
-import GamePage from "./pages/GamePage"
-import LobbyPage from "./pages/LobbyPage"
+import GamePage from "pages/GamePage"
+import LoginPage from "pages/LoginPage"
+import LobbyPage from "pages/LobbyPage"
+import { useEffect } from "react"
+import { useLocalstorage } from "hooks/useLocalstorage"
 
 function App() {
+  const { load } = useLocalstorage()
+
+  useEffect(() => {
+    const userId = load("userId")
+
+    if (userId === null) {
+      redirect("/login")
+    }
+  }, [])
+
   return (
     <Router>
       <Routes>
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/lobby" element={<LobbyLayout />}>
           <Route path="gameSelect" element={<LobbyPage />} />
         </Route>
