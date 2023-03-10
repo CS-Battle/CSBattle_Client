c1d33bf1ed76a4901056b16ce17f6d68aa411457
wlsrn3684 <wlsrn3684@naver.com>
2023-03-10 22:42:53 +0900

    ✨ Redirect userId does not exist

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { redirect, BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 import GameLayout from "layout/GameLayout"
 import LobbyLayout from "layout/Lobby/LobbyLayout"
