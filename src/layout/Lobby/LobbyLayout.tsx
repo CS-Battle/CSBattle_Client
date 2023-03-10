b159fd179f0fcc0498529d272d8682c472dc2980
SeungHo0422 <songseungho9258@gmail.com>
2023-03-10 22:41:27 +0900

    ✨ Add Lobby Layout
    로비페이지 레이아웃 추가

--- src/layout/Lobby/LobbyLayout.tsx
+++ src/layout/Lobby/LobbyLayout.tsx
@@ -4,20 +4,18 @@ import Footer from "./Footer"
 
 const LobbyLayout = () => {
   return (
-    <>
-      <div className="relative min-h-screen justify-center xl:justify-start overflow-hidden bg-OnPrimaryContainer px-20 py-10">
-        <div className="relative bg-white flex flex-col aspect-[16/7] max-xl  px-1 py-1 justify-center shadow-xl ring-8 ring-Secondary rounded-lg">
-          <Header />
-          <div className="flex-1">
-            <Outlet />
-          </div>
-        </div>
-
-        <div className="relative flex-none px-7 py-7 justify-center">
-          <Footer />
+    <div className="relative min-h-screen justify-center xl:justify-start overflow-hidden bg-OnPrimaryContainer px-20 py-10">
+      <div className="relative bg-white flex flex-col aspect-[16/7] max-xl  px-1 py-1 justify-center shadow-xl ring-8 ring-Secondary rounded-lg">
+        <Header />
+        <div className="flex-1">
+          <Outlet />
         </div>
       </div>
-    </>
+
+      <div className="relative flex-none px-7 py-7 justify-center">
+        <Footer />
+      </div>
+    </div>
   )
 }
 
