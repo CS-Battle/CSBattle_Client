7dfd01294e5636ac5a114e1d7dd2ca8d9710cfab
SeungHo0422 <songseungho9258@gmail.com>
2023-03-12 19:04:35 +0900

    ⚡️ Abstraction Components

--- src/pages/LobbyPage.tsx
+++ src/pages/LobbyPage.tsx
@@ -1,7 +1,8 @@
-import React from "react"
-import Button from "../components/atoms/Button/Button"
-import * as Label from "@radix-ui/react-label"
-import {Dialog, DialogTrigger, DialogContent } from "../components/atoms/Dialog/Dialog"
+import Button from "components/atoms/Button/Button"
+import { Dialog, DialogTrigger } from "components/atoms/radix/DialogContent/DialogContent"
+import Label from "components/atoms/radix/Label/Label"
+import ConfirmDialogContent from "components/molecules/ConfirmDialogContent/ConfirmDialogContent"
+import GameButtonList from "components/molecules/GameButtonList/GameButtonList"
 
 const LobbyPage = () => {
   return (
@@ -23,14 +24,16 @@ const LobbyPage = () => {
           </div>
         </div>
 
-        <div className="self-end flex-initial">
-          <Dialog>
-            <DialogTrigger>
-              <Button className="relative mx-12">게임 시작</Button>
-            </DialogTrigger>
-            <DialogContent></DialogContent>
-          </Dialog>
-        </div>
+      <div className="self-end flex-initial">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="relative mx-12">게임 시작</Button>
+          </DialogTrigger>
+          <ConfirmDialogContent
+            title = "게임 유형 선택"
+            description = "해당 유형으로 게임을 시작하시겠습니까?"
+          />
+        </Dialog>
       </div>
     </>
   )
