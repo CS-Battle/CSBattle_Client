a803b6cbec73758193e17b2ea16d8bcc5926fcad
SeungHo0422 <songseungho9258@gmail.com>
2023-03-13 03:13:54 +0900

    ♻️ Refactor ConfirmDialog

--- src/pages/LobbyPage.tsx
+++ src/pages/LobbyPage.tsx
@@ -1,9 +1,17 @@
 import Button from "components/atoms/Button/Button"
-import { Dialog, DialogTrigger } from "components/atoms/radix/DialogContent/DialogContent"
 import Label from "components/atoms/radix/Label/Label"
-import ConfirmDialogContent from "components/molecules/ConfirmDialogContent/ConfirmDialogContent"
+import ConfirmDialog from "components/molecules/ConfirmDialog/ConfirmDialog"
 import GameButtonList from "components/molecules/GameButtonList/GameButtonList"
 
+// 추후 const 데이터 모듈화 처리 필요
+const buttonLists = [
+  {
+    title: "매칭 시작"
+  },
+  {
+    title: "취소"
+  }
+]
 const LobbyPage = () => {
   return (
     <div className="flex flex-col justify-center h-full gap-12">
@@ -11,15 +19,13 @@ const LobbyPage = () => {
       <GameButtonList />
 
       <div className="self-end flex-initial">
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button className="relative mx-12">게임 시작</Button>
-          </DialogTrigger>
-          <ConfirmDialogContent
-            title = "게임 유형 선택"
-            description = "해당 유형으로 게임을 시작하시겠습니까?"
-          />
-        </Dialog>
+        <ConfirmDialog
+          title = "게임 유형 선택"
+          description = "해당 유형으로 게임을 시작하시겠습니까?"
+          buttonItems = {buttonLists}
+        >
+          <Button className="relative mx-12">게임 시작</Button>
+        </ConfirmDialog>
       </div>
     </div>
   )
