958fd0a54423350868b9b90be8bbe97c80592711
SeungHo0422 <songseungho9258@gmail.com>
2023-03-16 16:57:29 +0900

    ♻️ Refactore Footer CSS code

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -2,8 +2,8 @@ import { copyrights } from "./const"
 
 const Footer = () => {
   return (
-    <footer className="top-0 z-50 left-0 text-gray-700 body-font">
-      <h3 className="relative text-center scale-50 md:scale-25 md:text-sm text-white">{copyrights}</h3>
+    <footer className="top-0 left-0 z-50">
+      <h3 className="relative text-center text-white scale-50 md:scale-25 md:text-sm">{copyrights}</h3>
       {/* md 이하로 줄였을 때 맞춰서 footer 글자크기도 줄이고 싶은데, 안줄여지고 두줄처리되는 문제    */}
     </footer>
   )
