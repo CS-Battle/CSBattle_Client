8035daa23bb91ef50cebbcbc623b27253af36a34
SeungHo0422 <songseungho9258@gmail.com>
2023-03-16 18:48:43 +0900

    ⚡️ change Dialog to AlertDialog

--- src/pages/LobbyPage.tsx
+++ src/pages/LobbyPage.tsx
@@ -1,31 +1,28 @@
+import { useState } from "react"
 import Button from "components/atoms/Button/Button"
 import Label from "components/atoms/radix/Label/Label"
-import ConfirmDialog from "components/molecules/ConfirmDialog/ConfirmDialog"
+import GameselectAlertDialog from "components/molecules/GameselectAlertDialog/GameselectAtlertDialog"
 import GameButtonList from "components/molecules/GameButtonList/GameButtonList"
 
 // 추후 const 데이터 모듈화 처리 필요
-const buttonLists = [
-  {
-    title: "매칭 시작"
-  },
-  {
-    title: "취소"
-  }
-]
+
 const LobbyPage = () => {
+  const [gametype, setGametype] = useState<string>("")
+
   return (
     <div className="flex flex-col justify-center h-full gap-12">
       <Label>원하는 게임을 선택하세요!</Label>
       <GameButtonList />
 
       <div className="self-end flex-initial">
-        <ConfirmDialog
+        <GameselectAlertDialog
           title = "게임 유형 선택"
           description = "해당 유형으로 게임을 시작하시겠습니까?"
-          buttonItems = {buttonLists}
+          cancel = "취소"
+          action = "매칭 시작하기"
         >
           <Button className="relative mx-12">게임 시작</Button>
-        </ConfirmDialog>
+        </GameselectAlertDialog>
       </div>
     </div>
   )
