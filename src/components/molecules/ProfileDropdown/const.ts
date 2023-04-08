e67ff02020eb87c5687b0d3e90854129b104e2d7
SeungHo0422 <songseungho9258@gmail.com>
2023-04-08 23:25:39 +0900

    ✨ Add ProfileDropdown component

---
+++ src/components/molecules/ProfileDropdown/const.ts
@@ -0,0 +1,17 @@
+export const profileDropdownItems = [
+  {
+    title: "계정 설정",
+    url: "#",
+  },
+  {
+    title: "랭킹",
+    url: "#",
+  },
+  {
+    title: "커뮤니티",
+    url: "#",
+  },
+  {
+    title: "로그아웃"
+  }
+]
