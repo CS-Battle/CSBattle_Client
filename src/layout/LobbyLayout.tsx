11c332da7c48a43fbd3cd00d18c99ad7fb5e2299
SeungHo0422 <songseungho9258@gmail.com>
2023-04-08 19:43:14 +0900

    💄 Resize glassframe height

--- src/layout/LobbyLayout.tsx
+++ src/layout/LobbyLayout.tsx
@@ -5,14 +5,14 @@ import Footer from "components/organisms/Footer/Footer"
 const LobbyLayout = () => {
   return (
     <div className="background ">
-      <div className="glassframe">
+      <div className="w-full h-full max-w-6xl glassframe">
         <LobbyHeader />
         <div className="flex-1">
           <Outlet />
         </div>
       </div>
 
-      <div className="relative flex-none px-7 py-7 justify-center">
+      <div className="relative justify-center flex-none px-7 py-7">
         <Footer />
       </div>
     </div>
