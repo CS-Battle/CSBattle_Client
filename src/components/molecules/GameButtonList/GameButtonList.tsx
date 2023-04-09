f6076fad20145221f2ba79fa2679cb703ce073d4
SeungHo0422 <songseungho9258@gmail.com>
2023-04-09 18:12:27 +0900

    ⚡️ Add button color in focus

--- src/components/molecules/GameButtonList/GameButtonList.tsx
+++ src/components/molecules/GameButtonList/GameButtonList.tsx
@@ -1,6 +1,7 @@
 import Button from "components/atoms/Button/Button"
 import { GameTitleMap, gameList } from "./const"
-import { GameType, QuestionType } from "types/game"
+import { GameType } from "types/game"
+import { useEffect } from "react"
 
 interface GameButtonListProps {
   gametype: GameType
@@ -11,13 +12,17 @@ const GameButtonList = (props: GameButtonListProps) => {
   const { setGametype } = props
   const onClickButton = (gameType: GameType) => setGametype(gameType)
 
+
   return (
     <div className="justify-center flex-initial">
       <div className="grid grid-cols-3 gap-4">
         {gameList.map((game, i) => (
-          <Button key={i} className="glassbutton" onClick={() => onClickButton(game.type)}>
-            {GameTitleMap[game.type]}
-          </Button>
+            <Button
+              key={i}
+              className="glassbutton focus:bg-OnPrimaryVariantContainer focus:text-OnPrimary"
+              onClick={() => onClickButton(game.type)}
+            >{GameTitleMap[game.type]}</Button>
+
         ))}
       </div>
     </div>
