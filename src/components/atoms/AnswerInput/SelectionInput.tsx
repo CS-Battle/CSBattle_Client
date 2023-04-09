91c7dc447bda2b4849977d52c1bafacfe14b9977
SeungHo0422 <songseungho9258@gmail.com>
2023-04-09 22:40:03 +0900

    ♻️ Refactor code

--- src/components/atoms/AnswerInput/SelectionInput.tsx
+++ src/components/atoms/AnswerInput/SelectionInput.tsx
@@ -2,38 +2,31 @@ import { useState } from "react"
 import SelectionButton from "../SelectionButton.tsx/SelectionButton"
 
 interface SelectionInputProps {
-  description?: any
+  description: Array<any>
 }
 
 const SelectionInput = (props: SelectionInputProps) => {
   const { description } = props
-  const [isButtonSelect, setIsButtonSelect] = useState<any>(false)
+  const [isButtonSelect, setIsButtonSelect] = useState<Array<boolean>>(Array(description.length).fill(false))
 
-  const newArr = Array(description.length).fill(false)
   const handleClick = (idx: number) => {
-    newArr[idx] = true
-    setIsButtonSelect(newArr)
+    setIsButtonSelect((prev) => [...prev.slice(0, idx), !prev[idx], ...prev.slice(idx + 1)])
   }
 
-  // <Button className="glassbutton w-40 h-8 ">
-  //     <h1>{description}</h1>
-  //   </Button>
   return (
-    <>
-      <div className="flex w-full h-auto m-2 p-2 gap-2 justify-between">
-        {description.map((elm: any, index: number) => {
-          return (
-            <SelectionButton
-              key={index}
-              isSelected={isButtonSelect[index]}
-              handleClick={handleClick}
-              elementIndex={index}
-              content={elm}
-            />
-          )
-        })}
-      </div>
-    </>
+    <div className="flex justify-between w-full h-auto gap-2 p-2 m-2">
+      {description.map((elm: any, index: number) => {
+        return (
+          <SelectionButton
+            key={index}
+            isSelected={isButtonSelect[index]}
+            handleClick={handleClick}
+            elementIndex={index}
+            content={elm}
+          />
+        )
+      })}
+    </div>
   )
 }
 
