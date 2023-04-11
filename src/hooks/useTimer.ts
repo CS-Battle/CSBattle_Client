44084f7431e16697cd7b367948bfb40f90d3a830
SeungHo0422 <songseungho9258@gmail.com>
2023-04-12 03:35:36 +0900

    ⚡️ useTimer: countUp 기능 추가

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,19 +1,20 @@
 import { useEffect, useState } from "react"
 
-export const useTimer = (time: number) => {
+export const useTimer = (time: number, countup?: boolean) => {
+  const countUp = countup || false
   const [seconds, setSeconds] = useState<number>(time)
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (seconds <= 0) clearInterval(interval)
-      else setSeconds((seconds) => seconds - 1)
+      if (!countUp && seconds <= 0) clearInterval(interval)
+      else setSeconds((seconds) => (countUp ? seconds + 1 : seconds - 1))
     }, 1000)
-    if (seconds === 0) {
+    //TODO : countUp일 때 매칭성공 이벤트 응답 시 alert 띄우는 기능 추가
+    if (!countUp && seconds === 0) {
       alert("time out")
-      setSeconds(30)
+      setSeconds(time)
     }
     return () => clearInterval(interval)
-  }, [seconds])
-
+  }, [seconds, countUp, time])
   return { seconds }
 }
