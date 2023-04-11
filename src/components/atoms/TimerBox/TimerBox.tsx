250bad4a8ebafaa204c6dd37ebe4bfe1461f1ba0
SeungHo0422 <songseungho9258@gmail.com>
2023-04-12 03:34:52 +0900

    🚚  Timer->TimerBox로 컴포넌트명 수정

---
+++ src/components/atoms/TimerBox/TimerBox.tsx
@@ -0,0 +1,20 @@
+import { useTimer } from "hooks/useTimer"
+import Timer from "../Timer/Timer"
+
+interface TimerBoxProps {
+  time: number
+  countup? : boolean
+}
+const TimerBox = (props: TimerBoxProps) => {
+  const { time, countup=false } = props
+  const { seconds } = useTimer(time, countup)
+
+  return (
+    <div className="grid w-10 h-10 border rounded-full bg-OnPrimary place-items-center border-OnPrimaryContainer">
+      {/* <p className="text-base text-red-400">{seconds}</p> */}
+      <Timer time={seconds} color="red-400" countup={countup}/>
+    </div>
+  )
+}
+
+export default TimerBox
