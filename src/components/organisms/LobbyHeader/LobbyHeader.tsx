a377e99c9c1ac348ce539f0c538367166b462b1a
wlsrn3684 <wlsrn3684@naver.com>
2023-04-16 13:17:25 +0900

    ♻️ Edit asset import method

--- src/components/organisms/LobbyHeader/LobbyHeader.tsx
+++ src/components/organisms/LobbyHeader/LobbyHeader.tsx
@@ -1,27 +1,29 @@
-import {useContext, useEffect} from 'react'
+import { useContext, useEffect } from "react"
 import ProfileDropdown from "components/molecules/ProfileDropdown/ProfileDropdown"
 import { useLocalstorage } from "hooks/useLocalstorage"
 import Timer from "components/atoms/Timer/Timer"
-import { LobbyContext } from 'layout/LobbyLayout'
+import { LobbyContext } from "layout/LobbyLayout"
+
+import LogoImage from "assets/images/logo.png"
 
 const LobbyHeader = () => {
   const { load } = useLocalstorage()
-  const {showTimer} = useContext(LobbyContext)
+  const { showTimer } = useContext(LobbyContext)
   const userName = load("userId")
 
   // 새로고침 방지
   const preventReload = (e: BeforeUnloadEvent) => {
     e.preventDefault()
-    e.returnValue = ''
+    e.returnValue = ""
   }
 
   useEffect(() => {
-    (() => {
-      window.addEventListener('beforeunload', preventReload)
-    })();
+    ;(() => {
+      window.addEventListener("beforeunload", preventReload)
+    })()
 
     return () => {
-      window.removeEventListener('beforeunload', preventReload)
+      window.removeEventListener("beforeunload", preventReload)
     }
   }, [])
 
@@ -29,15 +31,15 @@ const LobbyHeader = () => {
     <header className="p-3 pt-8 m-2 border-b-4 border-b-BackgroundBorder">
       <nav className="flex items-center justify-start max-h-20">
         <div className="flex-none">
-          <img className="" alt="CS Battle" src="images/CSBattle_logo_2.png"/>
+          <img className="" alt="CS Battle" src={LogoImage} />
         </div>
-        {showTimer &&
+        {showTimer && (
           <div className="flex flex-none mx-8">
             <label>상대방을 기다리는 중 ... </label>
             <Timer className="ml-4" time={0} countup={true} />
             <label>초</label>
           </div>
-        }
+        )}
         <div className="flex-none ml-auto">
           <ProfileDropdown name={userName} />
         </div>
