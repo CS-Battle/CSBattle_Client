d0a4eace969e4f2628f572bc3b39c710158a2df7
ymj307 <dymj307@gmail.com>
2023-05-21 04:30:20 +0900

    :bug: fix the repeated alert when time is out
    fix the repeated alert when time is out

--- src/layout/GameLayout.tsx
+++ src/layout/GameLayout.tsx
@@ -1,9 +1,7 @@
-import Avatar from "ui/base/atoms/Avatar/Avatar"
-import TimerBox from "ui/base/molecules/TimerBox/TimerBox"
-import Statusbar from "ui/base/atoms/Statusbar/Statusbar"
 import Footer from "ui/base/organisms/Footer/Footer"
 import { Outlet } from "react-router-dom"
 import { useState, useEffect } from "react"
+import GameStatus from "ui/game/molecules/GameStatus/GameStatus"
 
 interface GameLayoutProps {
   children?: React.ReactElement
@@ -11,24 +9,26 @@ interface GameLayoutProps {
 
 const GameLayout = (props: GameLayoutProps) => {
   const { children } = props
-  //for testing
-  const time1 = 10
-  const time2 = 50
+  const time1 = 5
+  const time2 = 10
 
+  //for loading overlay
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [remainingTime, setRemainingTime] = useState<number>(time1)
 
   useEffect(() => {
-    const timer = setInterval((prevTime) => {
+    const timer = setInterval(() => {
       setRemainingTime((prevTime) => prevTime - 1)
     }, 1000)
 
-    if (remainingTime <= 0) {
-      setIsLoading(false)
-      clearInterval(timer)
-    }
     return () => {
       clearInterval(timer)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (remainingTime <= 0) {
+      setIsLoading(false)
     }
   }, [remainingTime])
 
@@ -36,28 +36,12 @@ const GameLayout = (props: GameLayoutProps) => {
     <div className="background">
       {isLoading ? (
         <div className="overlay">
-          <div className="timerText">Game will start in</div>
+          <div className="timerText">Game will be start after</div>
           <div className="timerText">{remainingTime} seconds</div>
-
-          {/* <div className="text-white">Loading... </div> */}
         </div>
       ) : (
         <div className="w-full h-auto max-w-6xl glassframe">
-          <div className="statusbox">
-            <div className="flex flex-row items-center relative">
-              <Avatar name={"user1"} image="https://source.boringavatars.com/beam/80/" />
-              <div className="-z-10 absolute left-16">
-                <Statusbar num={1} />
-              </div>
-            </div>
-            <TimerBox time={time2} />
-            <div className="flex z-10 flex-row items-center relative">
-              <div className="-z-10 absolute right-16">
-                <Statusbar num={2} />
-              </div>
-              <Avatar name={"user1"} image="https://source.boringavatars.com/beam/80" />
-            </div>
-          </div>
+          <GameStatus time={time2} />
           <div>{children || <Outlet />}</div>
         </div>
       )}
--- src/ui/game/molecules/GameStatus/GameStatus.tsx
+++ src/ui/game/molecules/GameStatus/GameStatus.tsx
@@ -1,15 +1,28 @@
-import Statusbar from "ui/base/atoms/Statusbar/Statusbar"
-import TimerBox from "ui/base/molecules/TimerBox/TimerBox"
 import Avatar from "ui/base/atoms/Avatar/Avatar"
+// import TimerBox from "ui/base/molecules/TimerBox/TimerBox"
+import Statusbar from "ui/base/atoms/Statusbar/Statusbar"
+import Timer from "ui/base/atoms/Timer/Timer"
 
-const GameStatus = () => {
+interface StatusBoxProps {
+  time: number
+}
+
+const GameStatus = ({ time }: StatusBoxProps) => {
   return (
-    <div className="">
-      {/* <Avatar name={"user1"} />
-      <Statusbar num={1} />
-      <TimerBox time={30} />
-      <Statusbar num={2} />
-      <Avatar name={"user2"} /> */}
+    <div className="statusbox">
+      <div className="flex flex-row items-center relative">
+        <Avatar name={"user1"} image="https://source.boringavatars.com/beam/80/" />
+        <div className="-z-10 absolute left-16">
+          <Statusbar num={1} />
+        </div>
+      </div>
+      <Timer time={time} color="timerText" />
+      <div className="flex z-10 flex-row items-center relative">
+        <div className="-z-10 absolute right-16">
+          <Statusbar num={2} />
+        </div>
+        <Avatar name={"user1"} image="https://source.boringavatars.com/beam/80" />
+      </div>
     </div>
   )
 }
